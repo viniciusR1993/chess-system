0759fc7b6c4570287c6b9a6cd467516b3b9e0859
Vinicius Ramalho Araujo <viniciusr@bb.com.br>
2020-08-30 15:19:58 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -13,11 +15,12 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while(true) {
 			try {
 				UI.clearScreen();	//Limpa a tela
-				UI.printMatch(chessMatch);	//Imprime o tabuleiro	
+				UI.printMatch(chessMatch, captured);	//Imprime o tabuleiro	
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);	//Lê a peça de captura
@@ -31,6 +34,10 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);	//Lê a posição desejada
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);	//Move a peça para o local desejado
+				
+				if(capturedPiece != null) {		//Se a peça capturad for diferente de nula, ele adiciona a lista
+					captured.add(capturedPiece);
+				}
 			}catch(ChessException e) {	//Com esse tratamento o programa não quebra, continua funcionando
 				System.out.println(e.getMessage());
 				sc.nextLine();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 
 	public ChessMatch() { // Esse é o construtor do tabuleiro dessa forma tem que chamar os outros metodos
 		board = new Board(8, 8);
@@ -54,10 +60,16 @@ public class ChessMatch {
 		return (ChessPiece)capturedPiece;
 	}
 	
-	private Piece makeMove(Position source, Position target) {
+	private Piece makeMove(Position source, Position target) {	//Move a peça da origem pro destino
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {	//retira da lista de peça do tabuleiro e adiciona nas capturadas, caso tenha peça capturada
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
+		
 		return capturedPiece;
 	}
 	
@@ -84,9 +96,9 @@ public class ChessMatch {
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 
-	private void placeNewPiece(char column, int row, ChessPiece piece) { // Esse metodo captura o que o usuario digita
-																			// para decopliar para o programa
+	private void placeNewPiece(char column, int row, ChessPiece piece) { // Esse metodo captura o que o usuario digita. Para decopliar para o programa
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 
 	public void initialSetup() { // Methodo que seta as peças no tabuleiro
